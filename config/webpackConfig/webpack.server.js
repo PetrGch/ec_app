349ca00c225d853b95e043f8f3f46da2e4d72d36
Petr <petr.gulchuk@gmail.com>
2018-09-03 06:23:38 +0200

    add ddos limitter

--- config/webpackConfig/webpack.server.js
+++ config/webpackConfig/webpack.server.js
@@ -7,66 +7,68 @@ const paths = require('./util/paths');
 process.env.NODE_PATH = (process.env.NODE_PATH || '');
 
 module.exports = {
-    entry: paths.serverIndexJs,
+  entry: paths.serverIndexJs,
 
-    output: {
-        path: paths.appPublic,
-        filename: 'serverBundle.js'
-    },
+  output: {
+    path: paths.appPublic,
+    filename: 'serverBundle.js'
+  },
 
-    target: 'node',
+  target: 'node',
 
-    externals: [nodeExternals()],
+  externals: [nodeExternals()],
 
-    resolve: {
-        extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx']
-    },
+  resolve: {
+    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx']
+  },
 
-    module: {
-        strictExportPresence: true,
-        rules: [
-            {
-                oneOf: [
-                    {
-                        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-                        loader: require.resolve('url-loader'),
-                        options: {
-                            limit: 10000,
-                            name: 'static/media/[name].[ext]',
-                        },
-                    },
-                    {
-                        test: /\.(js|jsx|mjs)$/,
-                        use: [
-                            {
-                                loader: 'babel-loader',
-                                options: {
-                                    cacheDirectory: true
-                                }
-                            },
+  watch: true,
 
-                        ],
-                    },
-                    {
-                        exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
-                        loader: require.resolve('file-loader'),
-                        options: {
-                            name: 'static/media/[name].[ext]',
-                        },
-                    }
-                ]
-            }
+  module: {
+    strictExportPresence: true,
+    rules: [
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('url-loader'),
+            options: {
+              limit: 10000,
+              name: 'static/media/[name].[ext]',
+            },
+          },
+          {
+            test: /\.(js|jsx|mjs)$/,
+            use: [
+              {
+                loader: 'babel-loader',
+                options: {
+                  cacheDirectory: true
+                }
+              },
+
+            ],
+          },
+          {
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            loader: require.resolve('file-loader'),
+            options: {
+              name: 'static/media/[name].[ext]',
+            },
+          }
         ]
-    },
-
-    node: {
-        __filename: false,
-        __dirname: false
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            __isBrowser__: "false"
-        })
+      }
     ]
+  },
+
+  node: {
+    __filename: false,
+    __dirname: false
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "false"
+    })
+  ]
 };
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 import path from 'path';
 import fs from 'fs';
+import express from 'express';
 
 import React from 'react';
-import express from 'express';
 import ReactDOMServer from 'react-dom/server';
 import App from "../src/app/App";
 import {StaticRouter} from "react-router-dom";
 import configureStore from "../src/store/configureStore";
 import {Provider} from "react-redux";
+
+import expressRateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,31 +19,59 @@ const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 const sourceDirectory = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
 
-app.use(express.static(resolveApp(`./public/${sourceDirectory}`)));
+// --------- express-rate-limit -----------
+const indexHtml = expressRateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100,
+  message: "WTF!!! Stop this shit",
+  skipFailedRequests: true,
+  // handler: function (req, res, /*next*/) {
+  //   res.status(500).send("WTF!!!");
+  // },
+  onLimitReached: function (req, res, options) {
+    saveLog("ddos", `id: ${req.ip} - limit reached`);
+  },
+  // store: new RedisStore({
+  //   // see Configuration
+  // }),
+});
+app.use("/*", indexHtml);
+// --------- express-rate-limit -----------
 
-app.get('/*', (req, res) => {
-    console.log(sourceDirectory);
-    const context = {};
-    const app = ReactDOMServer.renderToString(
-        <Provider store={store}>
-            <StaticRouter location={req.url} context={context}>
-                <App />
-            </StaticRouter>
-        </Provider>
+app.use("/static", express.static(resolveApp(`./public/${sourceDirectory}/static/`)));
+
+function saveLog (nick, command) {
+  const file = `${nick}.log`;
+  const datetime = '[' + new Date() + '] ';
+  const text = datetime + command + '\r\n';
+  fs.appendFile(file, text, function (err) {
+    if (err) return console.log(err);
+    console.log('successfully appended "' + text + '"');
+  });
+}
+
+app.get('*', (req, res) => {
+  const context = {};
+  const app = ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={context}>
+        <App/>
+      </StaticRouter>
+    </Provider>
+  );
+  const indexFile = resolveApp(`./public/${sourceDirectory}/index.html`);
+  fs.readFile(indexFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
+    }
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
     );
-    const indexFile = resolveApp(`./public/${sourceDirectory}/index.html`);
-    fs.readFile(indexFile, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Something went wrong:', err);
-            return res.status(500).send('Oops, better luck next time!');
-        }
-
-        return res.send(
-            data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-        );
-    });
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`😎 Server is listening on port ${PORT}`);
+  console.log(`😎 Server is listening on port ${PORT}`);
 });
