8d0a8d858717e38e5c696a7aed9b950e8a1afa80
Petr <petr.gulchuk@gmail.com>
2018-11-25 01:57:34 +0100

    fixed server index file

--- server/index.js
+++ server/index.js
@@ -105,7 +105,7 @@ app.get('*', (req, res) => {
   const helmet = Helmet.renderStatic();
 
   const indexFile = resolveApp(`./dist/indexRoot.html`);
-  saveLog(logs, indexFile);
+  saveLog("logs", indexFile);
   fs.readFile(indexFile, 'utf8', (err, data) => {
     if (err) {
       console.error('Something went wrong:', err);
